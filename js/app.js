eaadca96d5d9fd6860d4f3ae06a3047642eb5a5a
santiago <saforero88@gmail.com>
2021-03-12 17:53:03 -0500

    computed & methods

--- js/app.js
+++ js/app.js
@@ -2,7 +2,19 @@ const app = new Vue({
     el: '#app',
     data: {
         message: "Hola Vue",
-        sum: 3 + 6
+        sum: 3 + 6,
+        value: '',
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function() {
+            // `this` points to the vm instance
+            return this.message.split('').reverse().join('')
+        }
+    },
+    methods: {
+        calculateSum(value1, value2) {
+            return value1 + value2;
+        }
     }
-
 });
