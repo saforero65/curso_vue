b0fc2a9c2b67517a3de5fd53fe91c2b1488b069e
santiago <saforero88@gmail.com>
2021-03-12 23:34:55 -0500

    primeros pasos con vue, instalacion

---
+++ 1. Primeros Pasos/js/app.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hola Vue",
+        sum: 3 + 6,
+        value: '',
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function() {
+            // `this` points to the vm instance
+            return this.message.split('').reverse().join('')
+        }
+    },
+    methods: {
+        calculateSum(value1, value2) {
+            return value1 + value2;
+        }
+    }
+});
