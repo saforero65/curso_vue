11d1fb2c2efac307a40edf0f0eff992c41550c4b
santiago <saforero88@gmail.com>
2021-03-12 23:18:54 -0500

    events

---
+++ events/js/app.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hola Vue",
+        sum: 3 + 6,
+        value: '9',
+        isVisible: true,
+        people: ['Andres', 'Juan', 'Pepe', 'Alejando'],
+        numero: '10'
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function() {
+            // `this` points to the vm instance
+            return this.message.split('').reverse().join('')
+        }
+    },
+    methods: {
+        calculateSum(value1, value2) {
+            return value1 + value2;
+        },
+        incrementar() {
+            this.numero++;
+        },
+        decrementar() {
+            this.numero--;
+        }
+    }
+});
