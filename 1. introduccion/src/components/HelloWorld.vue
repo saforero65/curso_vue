246fb8965d18cf64fff1b1f090eb4e8c2431553a
santiago <saforero88@gmail.com>
2021-03-12 23:29:46 -0500

    .

---
+++ 1. introduccion/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ text }}</h2>
+    <div style="background: red">
+      <MiComponente></MiComponente>
+    </div>
+  </div>
+</template>
+
+<script>
+// import MiComponente from "../components/MiComponente.vue";
+import MiComponente from "./MiComponente.vue";
+export default {
+  name: "HelloWorld",
+  components: {
+    MiComponente,
+  },
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      text: "Hola Mundo Vue!!",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
