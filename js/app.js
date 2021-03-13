d143727ad8a52c1285ea7bc128a4486c164cd29e
santiago <saforero88@gmail.com>
2021-03-13 00:28:25 -0500

    modelos

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hola Vue",
+        sum: 3 + 6,
+        value: '9',
+        isVisible: true,
+        people: ['Andres', 'Juan', 'Pepe', 'Alejando'],
+        numero: 10,
+        p_error: false,
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function() {
+            // `this` points to the vm instance
+            return this.message.split('').reverse().join('')
+        }
+    },
+    methods: {
+        calculateSum(value1, value2) {
+            return value1 + value2;
+        },
+        incrementar() {
+            this.numero++;
+        },
+        decrementar() {
+            this.numero--;
+        }
+    },
+    watch: {
+        numero: function(val) {
+            console.log("watch:" + val);
+        }
+    }
+});
