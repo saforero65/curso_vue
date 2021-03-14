46b303256d3c756e63ca5181fb4d6db646c4e383
Santiago <saforero88@gmail.com>
2021-03-14 14:33:44 -0500

    slot

--- 7.rutas/js/components/Slot.js
+++ 7.rutas/js/components/Slot.js
@@ -4,15 +4,17 @@ Vue.component('modal-bootsrap', {
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <slot name="header"></slot>
+          
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <slot></slot>
+          <slot></slot>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Understood</button>
+          
+          <slot name="footer"></slot>
+           
           </div>
         </div>
       </div>
