da98cd7745c812004c9e9490b5df05a7f0bdca06
Santiago <saforero88@gmail.com>
2021-03-14 16:24:39 -0500

    filtro con input

--- 2. For con Vue/js/app.js
+++ 2. For con Vue/js/app.js
@@ -5,7 +5,21 @@ const app = new Vue({
         sum: 3 + 6,
         value: '9',
         isVisible: true,
+        filtro_edad: 25,
         people: ['Andres', 'Juan', 'Pepe', 'Alejando'],
+        CopyArrayObjets: [],
+        ArrayObjets: [
+            { name: "andres", surname: "cruz", age: 28 },
+            { name: "fernadn", surname: "paco", age: 18 },
+            { name: "valeria", surname: "manfg", age: 25 },
+            { name: "nancy", surname: "mora", age: 24 },
+            { name: "angelica", surname: "forero", age: 258 },
+            { name: "santiago", surname: "segio", age: 268 },
+            { name: "jesus", surname: "alvaro", age: 2108 },
+        ],
+    },
+    created: function() {
+        this.CopyArrayObjets = this.ArrayObjets;
     },
     computed: {
         // a computed getter
@@ -15,6 +29,14 @@ const app = new Vue({
         }
     },
     methods: {
+        filterPeopleObjets: function() {
+            this.ArrayObjets = this.CopyArrayObjets;
+            this.ArrayObjets = this.ArrayObjets.filter(
+                person => {
+                    return person.age > this.filtro_edad
+                }
+            )
+        },
         filterPeople: function() {
             this.people = this.people.filter(person => {
                 return person.length > 5
